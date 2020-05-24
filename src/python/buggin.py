98544f20aaa6b098c4e74a606fe9126d6eaac122
Phil McGee <p27mcgee@users.noreply.github.com>
2020-05-23 20:24:41 -0400

    separate results dir

--- src/python/buggin.py
+++ src/python/buggin.py
@@ -4,14 +4,14 @@ from os import path
 
 SKEWER = "-"
 
-def makeLogPath(data_dir, log_ext, *signature_names):
+def makeLogPath(dir, log_ext, *signature_names):
     filename = ""
     for signature_name in signature_names:
         if filename:
             filename += SKEWER
         filename += signature_name
     filename += log_ext
-    return path.join(data_dir, filename)
+    return path.join(dir, filename)
 
 def filter_log(infile, outfile, filter):
     with open(infile, 'r') as inlog:
@@ -41,8 +41,8 @@ def applyFilters(line, signature_map, *signature_names):
         negate_next_filter = False
     return result
 
-def call_filter_log(src_log, data_dir, log_ext, signature_map, *signature_names):
-    filter_log(src_log, makeLogPath(data_dir, log_ext, *signature_names), lambda line: applyFilters(line, signature_map, *signature_names))
+def call_filter_log(src_log, output_dir, log_ext, signature_map, *signature_names):
+    filter_log(src_log, makeLogPath(output_dir, log_ext, *signature_names), lambda line: applyFilters(line, signature_map, *signature_names))
     print()
 
 NOT = "not"
@@ -66,7 +66,8 @@ signature_map = {
     NULL_LOC: "&location=null"
 }
 
-data_dir = "/Volumes/GoogleDrive/My Drive/work/PycharmProjects/debug-logs/data/"
+data_dir = "/Users/philmcgee/GitHub/debug-log/data"
+results_dir = "/Users/philmcgee/GitHub/debug-log/results"
 log_ext = ".log"
 debug_log = makeLogPath(data_dir, log_ext, "petclinic")
 
@@ -87,16 +88,7 @@ logs_signature_names = [
 
 def generate_filtered_logs(logs_signature_names):
     for signature_names in logs_signature_names:
-        call_filter_log(debug_log, data_dir, log_ext, signature_map, *signature_names)
+        call_filter_log(debug_log, results_dir, log_ext, signature_map, *signature_names)
 
 generate_filtered_logs(logs_signature_names)
-#
-# nullname_loads = "/Volumes/GoogleDrive/My Drive/work/PycharmProjects/debug-logs/data/nullname-loads.log"
-# platform_loads = "/Volumes/GoogleDrive/My Drive/work/PycharmProjects/debug-logs/data/platform-loads.log"
-# app_loads = "/Volumes/GoogleDrive/My Drive/work/PycharmProjects/debug-logs/data/app-loads.log"
-# not_null_platform_app_loads = "/Volumes/GoogleDrive/My Drive/work/PycharmProjects/debug-logs/data/not-null-platform-app-loads.log"
-# filter_log(not_nullname_notnullpd_loads, nullname_loads, lambda line: "|classloader=null&" in line)
-# filter_log(not_nullname_notnullpd_loads, platform_loads, lambda line: "|classloader=platform&" in line)
-# filter_log(not_nullname_notnullpd_loads, app_loads, lambda line: "|classloader=app&" in line)
-# filter_log(not_nullname_notnullpd_loads, not_null_platform_app_loads, lambda line: not "|classloader=null&" in line and not "|classloader=platform&" in line and not "|classloader=app&" in line)
-# print()
+
